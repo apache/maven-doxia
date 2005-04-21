f3440a6202729209b70680962ac6a35097f2ebb3
Emmanuel Venisse <evenisse@apache.org>
2005-04-21 21:18:15 +0000

    Fix xml.
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia-core/trunk@331438 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/codehaus/doxia/module/xhtml/codehaus/CodehausXhtmlSink.java
+++ src/main/java/org/codehaus/doxia/module/xhtml/codehaus/CodehausXhtmlSink.java
@@ -85,7 +85,7 @@ public class CodehausXhtmlSink
 
         try
         {
-            writer.write( "<?xml version=\"1.0\">\n" );
+            writer.write( "<?xml version=\"1.0\"?>\n" );
 
             writer
                 .write( "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1-transitional.dtd\">\n" );
