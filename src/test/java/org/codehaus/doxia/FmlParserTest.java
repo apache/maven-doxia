7dc1f56051fef167dcb336d5525109cdf4aa77d0
Emmanuel Venisse <evenisse@apache.org>
2005-04-23 00:18:42 +0000

    Add an AbstractParserTestCase class
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia-core/trunk@331447 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/codehaus/doxia/FmlParserTest.java
+++ src/test/java/org/codehaus/doxia/FmlParserTest.java
@@ -1,51 +1,23 @@
 package org.codehaus.doxia;
 
 import org.codehaus.doxia.module.fml.FmlParser;
-import org.codehaus.doxia.module.xhtml.decoration.render.RenderingContext;
-import org.codehaus.doxia.module.xhtml.codehaus.CodehausXhtmlSink;
-import org.codehaus.doxia.module.xhtml.decoration.model.DecorationModel;
-import org.codehaus.doxia.module.xhtml.decoration.model.DecorationModelReader;
-import org.codehaus.doxia.sink.Sink;
-import junit.framework.TestCase;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-import java.io.Writer;
+import org.codehaus.doxia.parser.Parser;
+import org.codehaus.doxia.parser.AbstractParserTestCase;
 
 /**
  * @author <a href="mailto:evenisse@codehaus.org">Jason van Zyl</a>
  * @version $Id: XdocParserTest.java,v 1.4 2004/09/14 14:26:38 jvanzyl Exp $
  */
 public class FmlParserTest
-    extends TestCase
+    extends AbstractParserTestCase
 {
-    public void testApt()
-        throws Exception
+    protected Parser getParser()
     {
-        String basedir = System.getProperty( "basedir" );
+        return new FmlParser();
+    }
 
-        String siteXml = new File( basedir, "src/test/site/site.xml").getPath();
-
-        FmlParser fmlParser = new FmlParser();
-
-        Reader reader = new FileReader( new File( basedir, "src/test/site/fml/faq.fml" ) );
-
-        Writer writer = new FileWriter( "faq.html" );
-
-        String xdoc = "repository-upload.xml";
-
-        DecorationModelReader b = new DecorationModelReader();
-
-        DecorationModel navigation = b.createNavigation( siteXml );
-
-        RenderingContext renderingContext = new RenderingContext( new File( basedir ), 
-                                                                  new File( basedir, xdoc ).getPath(),
-                                                                  navigation );
-
-        Sink sink = new CodehausXhtmlSink( writer, renderingContext );
-
-        fmlParser.parse( reader, sink );
+    protected String getDocument()
+    {
+        return "src/test/site/fml/faq.fml";
     }
 }
--- src/test/java/org/codehaus/doxia/XdocParserTest.java
+++ src/test/java/org/codehaus/doxia/XdocParserTest.java
@@ -1,50 +1,24 @@
 package org.codehaus.doxia;
 
 import org.codehaus.doxia.module.xdoc.XdocParser;
-import org.codehaus.doxia.module.xhtml.decoration.render.RenderingContext;
-import org.codehaus.doxia.module.xhtml.codehaus.CodehausXhtmlSink;
-import org.codehaus.doxia.module.xhtml.decoration.model.DecorationModel;
-import org.codehaus.doxia.module.xhtml.decoration.model.DecorationModelReader;
-import org.codehaus.doxia.sink.Sink;
-import junit.framework.TestCase;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-import java.io.Writer;
+import org.codehaus.doxia.parser.Parser;
+import org.codehaus.doxia.parser.AbstractParserTestCase;
 
 /**
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id: XdocParserTest.java,v 1.4 2004/09/14 14:26:38 jvanzyl Exp $
  */
 public class XdocParserTest
-    extends TestCase
+    extends AbstractParserTestCase
 {
-    public void testApt() throws Exception
+    protected Parser getParser()
     {
-        String basedir = System.getProperty( "basedir" );
+        return new XdocParser();
+    }
 
-        String siteXml = new File( basedir, "src/test/site/site.xml" ).getPath();
-
-        XdocParser xdocParser = new XdocParser();
-
-        Reader reader = new FileReader( new File( basedir, "src/test/site/xdoc/report.xml" ) );
-
-        Writer writer = new FileWriter( "repository-upload.html" );
-
-        String xdoc = "repository-upload.xml";
-
-        DecorationModelReader b = new DecorationModelReader();
-
-        DecorationModel navigation = b.createNavigation( siteXml );
-
-        RenderingContext renderingContext = new RenderingContext( new File( basedir ),
-                                                                  new File( basedir, xdoc ).getPath(),
-                                                                  navigation );
-
-        Sink sink = new CodehausXhtmlSink( writer, renderingContext );
-
-        xdocParser.parse( reader, sink );
+    protected String getDocument()
+    {
+        return "src/test/site/xdoc/report.xml";
     }
 }
