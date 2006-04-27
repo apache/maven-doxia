d383881eecc60e31a5faacf0014a823369905924
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-04-27 21:53:12 +0000

    Format
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/trunk@397653 13f79535-47bb-0310-9956-ffa450edef68

--- doxia-core/src/test/java/org/apache/maven/doxia/module/rtf/RtfSinkTest.java
+++ doxia-core/src/test/java/org/apache/maven/doxia/module/rtf/RtfSinkTest.java
@@ -1,7 +1,7 @@
 package org.apache.maven.doxia.module.rtf;
 
 /*
- * Copyright 2004-2005 The Apache Software Foundation.
+ * Copyright 2004-2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ public class RtfSinkTest
         throws Exception
     {
         File outputFile = new File( getBasedirFile(), "target/output/test.rtf" );
-	outputFile.getParentFile().mkdirs();
+        outputFile.getParentFile().mkdirs();
         return new RtfSink( new FileOutputStream( outputFile ) );
     }
 }
