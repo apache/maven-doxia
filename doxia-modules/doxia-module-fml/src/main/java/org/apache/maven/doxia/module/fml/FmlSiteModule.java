b60c8a9ba1979edcd4450d427226f30d423606cb
Emmanuel Venisse <evenisse@apache.org>
2007-03-22 10:26:25 +0000

    o Fix fml module o fml module to site renderer
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia/trunk@521191 13f79535-47bb-0310-9956-ffa450edef68

--- doxia-modules/doxia-module-fml/src/main/java/org/apache/maven/doxia/module/fml/FmlSiteModule.java
+++ doxia-modules/doxia-module-fml/src/main/java/org/apache/maven/doxia/module/fml/FmlSiteModule.java
@@ -24,7 +24,7 @@ import org.apache.maven.doxia.site.module.AbstractSiteModule;
 /**
  * @author <a href="mailto:evenisse@codehaus.org">Emmanuel Venisse</a>
  * @version $Id:FmlSiteModule.java 348605 2005-11-24 12:02:44 +1100 (Thu, 24 Nov 2005) brett $
- * @plexus.component  role-hint="fml"
+ * @plexus.component role="org.apache.maven.doxia.site.module.SiteModule" role-hint="fml"
  */
 public class FmlSiteModule
     extends AbstractSiteModule
