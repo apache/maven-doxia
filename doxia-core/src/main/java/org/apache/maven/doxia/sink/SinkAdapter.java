d5d735441a09918f2ee73fbc0f03bc5128896764
Vincent Siveton <vsiveton@apache.org>
2007-06-05 03:07:46 +0000

    DOXIA-114: Adding Doxia API Javadoc
    o added javadoc
    o no code change
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia/trunk@544354 13f79535-47bb-0310-9956-ffa450edef68

--- doxia-core/src/main/java/org/apache/maven/doxia/sink/SinkAdapter.java
+++ doxia-core/src/main/java/org/apache/maven/doxia/sink/SinkAdapter.java
@@ -20,365 +20,722 @@ package org.apache.maven.doxia.sink;
  */
 
 /**
- * Empty implementation of the Sink interface. Useful for testing purposes.
+ * Empty implementation of the <code>Sink</code> interface. Useful for testing purposes.
+ *
+ * @since 1.0
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @author <a href="mailto:vincent.siveton@gmail.com">Vincent Siveton</a>
+ * @version $Id$
  */
 public class SinkAdapter
     implements Sink
 {
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#head()
+     */
     public void head()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#head_()
+     */
     public void head_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#body()
+     */
     public void body()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#body_()
+     */
     public void body_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section1()
+     */
     public void section1()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section1_()
+     */
     public void section1_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section2()
+     */
     public void section2()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section2_()
+     */
     public void section2_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section3()
+     */
     public void section3()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section3_()
+     */
     public void section3_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section4()
+     */
     public void section4()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section4_()
+     */
     public void section4_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section5()
+     */
     public void section5()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#section5_()
+     */
     public void section5_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#list()
+     */
     public void list()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#list_()
+     */
     public void list_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#listItem()
+     */
     public void listItem()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#listItem_()
+     */
     public void listItem_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#numberedList(int)
+     */
     public void numberedList( int numbering )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#numberedList_()
+     */
     public void numberedList_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#numberedListItem()
+     */
     public void numberedListItem()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#numberedListItem_()
+     */
     public void numberedListItem_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definitionList()
+     */
     public void definitionList()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definitionList_()
+     */
     public void definitionList_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definitionListItem()
+     */
     public void definitionListItem()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definitionListItem_()
+     */
     public void definitionListItem_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definition()
+     */
     public void definition()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definition_()
+     */
     public void definition_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#figure()
+     */
     public void figure()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#figure_()
+     */
     public void figure_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#table()
+     */
     public void table()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#table_()
+     */
     public void table_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableRows(int[], boolean)
+     */
     public void tableRows( int[] justification, boolean grid )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableRows_()
+     */
     public void tableRows_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableRow()
+     */
     public void tableRow()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableRow_()
+     */
     public void tableRow_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#title()
+     */
     public void title()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#title_()
+     */
     public void title_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#author()
+     */
     public void author()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#author_()
+     */
     public void author_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#date()
+     */
     public void date()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#date_()
+     */
     public void date_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle()
+     */
     public void sectionTitle()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle_()
+     */
     public void sectionTitle_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle1()
+     */
     public void sectionTitle1()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle1_()
+     */
     public void sectionTitle1_()
     {
+        // nop
     }
 
     public void sectionTitle2()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle2_()
+     */
     public void sectionTitle2_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle3()
+     */
     public void sectionTitle3()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle3_()
+     */
     public void sectionTitle3_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle4()
+     */
     public void sectionTitle4()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle4_()
+     */
     public void sectionTitle4_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle5()
+     */
     public void sectionTitle5()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#sectionTitle5_()
+     */
     public void sectionTitle5_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#paragraph()
+     */
     public void paragraph()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#paragraph_()
+     */
     public void paragraph_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#verbatim(boolean)
+     */
     public void verbatim( boolean boxed )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#verbatim_()
+     */
     public void verbatim_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definedTerm()
+     */
     public void definedTerm()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#definedTerm_()
+     */
     public void definedTerm_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#figureCaption()
+     */
     public void figureCaption()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#figureCaption_()
+     */
     public void figureCaption_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableCell()
+     */
     public void tableCell()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableCell(java.lang.String)
+     */
     public void tableCell( String width )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableCell_()
+     */
     public void tableCell_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableHeaderCell()
+     */
     public void tableHeaderCell()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableHeaderCell(java.lang.String)
+     */
     public void tableHeaderCell( String width )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableHeaderCell_()
+     */
     public void tableHeaderCell_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableCaption()
+     */
     public void tableCaption()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#tableCaption_()
+     */
     public void tableCaption_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#figureGraphics(java.lang.String)
+     */
     public void figureGraphics( String name )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#horizontalRule()
+     */
     public void horizontalRule()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#pageBreak()
+     */
     public void pageBreak()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#anchor(java.lang.String)
+     */
     public void anchor( String name )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#anchor_()
+     */
     public void anchor_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#link(java.lang.String)
+     */
     public void link( String name )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#link_()
+     */
     public void link_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#italic()
+     */
     public void italic()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#italic_()
+     */
     public void italic_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#bold()
+     */
     public void bold()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#bold_()
+     */
     public void bold_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#monospaced()
+     */
     public void monospaced()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#monospaced_()
+     */
     public void monospaced_()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#lineBreak()
+     */
     public void lineBreak()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#nonBreakingSpace()
+     */
     public void nonBreakingSpace()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#text(java.lang.String)
+     */
     public void text( String text )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#rawText(java.lang.String)
+     */
     public void rawText( String text )
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#flush()
+     */
     public void flush()
     {
+        // nop
     }
 
+    /**
+     * @see org.apache.maven.doxia.sink.Sink#close()
+     */
     public void close()
     {
+        // nop
     }
 }
-
