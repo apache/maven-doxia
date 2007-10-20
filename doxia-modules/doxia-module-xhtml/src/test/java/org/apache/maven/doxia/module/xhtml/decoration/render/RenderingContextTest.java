08f9e006490d6dee6d88ae1e8625c5dac4e3fc03
Oliver Lamy <olamy@apache.org>
2007-10-20 14:32:55 +0000

    [DOXIA-161] apply fix in branch
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia/branches/doxia-1.0-alpha-x@586732 13f79535-47bb-0310-9956-ffa450edef68

---
+++ doxia-modules/doxia-module-xhtml/src/test/java/org/apache/maven/doxia/module/xhtml/decoration/render/RenderingContextTest.java
@@ -0,0 +1,46 @@
+package org.apache.maven.doxia.module.xhtml.decoration.render;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+/**
+ * @author <a href="mailto:olamy@apache.org">olamy</a>
+ * @since 20 oct. 07
+ * @version $Id$
+ */
+public class RenderingContextTest
+    extends PlexusTestCase
+{
+
+    public void testFileNameWithDot()
+        throws Exception
+    {
+        File baseDir = new File( getBasedir() + File.separatorChar + "test" + File.separatorChar + "resources" );
+        String docName = "file.with.dot.in.name.xml";
+
+        RenderingContext renderingContext = new RenderingContext( baseDir, docName );
+        assertEquals( "file.with.dot.in.name.html", renderingContext.getOutputName() );
+        assertEquals( ".", renderingContext.getRelativePath() );
+    }
+    
+}
