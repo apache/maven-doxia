7668e1e654d4e6d2fd5a48cd3c1619a9fab66ac5
Vincent Siveton <vsiveton@apache.org>
2009-05-27 17:26:01 +0000

    DOXIA-325: Not valid xhtml document if comment with minus character
    o take care of minus char
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia/trunk@779250 13f79535-47bb-0310-9956-ffa450edef68

--- doxia-modules/doxia-module-xhtml/src/main/java/org/apache/maven/doxia/module/xhtml/XhtmlSink.java
+++ doxia-modules/doxia-module-xhtml/src/main/java/org/apache/maven/doxia/module/xhtml/XhtmlSink.java
@@ -241,6 +241,16 @@ public class XhtmlSink
         resetState();
     }
 
+    /** {@inheritDoc} */
+    public void comment( String comment )
+    {
+        if ( StringUtils.isNotEmpty( comment ) )
+        {
+            comment = StringUtils.replace( comment, "-", " -" );
+        }
+        super.comment( comment );
+    }
+
     // ----------------------------------------------------------------------
     // Public protected methods
     // ----------------------------------------------------------------------
