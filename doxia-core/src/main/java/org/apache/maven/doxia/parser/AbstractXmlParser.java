b15bc2c792fcc197c520ad41d0d01bc8e78a1e61
Dennis Lundberg <dennisl@apache.org>
2010-11-16 18:48:53 +0000

    [DOXIA-417] The XmlParser is blocked from W3C URLs
    o Change the user-agent name/version
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/doxia/doxia/trunk@1035744 13f79535-47bb-0310-9956-ffa450edef68

--- doxia-core/src/main/java/org/apache/maven/doxia/parser/AbstractXmlParser.java
+++ doxia-core/src/main/java/org/apache/maven/doxia/parser/AbstractXmlParser.java
@@ -800,7 +800,7 @@ public abstract class AbstractXmlParser
             HttpGet method = new HttpGet( url.toString() );
             // Set a user-agent that doesn't contain the word "java", otherwise it will be blocked by the W3C
             // The default user-agent is "Apache-HttpClient/4.0.2 (java 1.5)"
-            method.setHeader( "user-agent", "Apache-HttpClient/4.0.2" );
+            method.setHeader( "user-agent", "Apache-Doxia/1.1.4" );
 
             HttpRequestRetryHandler retryHandler = new DefaultHttpRequestRetryHandler( 3, false );
             client.setHttpRequestRetryHandler(retryHandler);
