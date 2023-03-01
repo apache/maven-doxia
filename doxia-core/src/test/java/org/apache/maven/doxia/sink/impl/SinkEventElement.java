37173794270ee68894aac0e252dcb87545770952
Konrad Windszus <kwin@apache.org>
2023-03-01 12:35:54 +0100

    [DOXIA-694] fix newlines (#146)
    Fix wrong markup (still APT format) for
    - Non-breaking space
    - Line break
    - Definition List
    - Horizontal Rule
    Remove inline styles from verbatim blocks
    Add roundtrip test

--- doxia-core/src/test/java/org/apache/maven/doxia/sink/impl/SinkEventElement.java
+++ doxia-core/src/test/java/org/apache/maven/doxia/sink/impl/SinkEventElement.java
@@ -80,4 +80,22 @@ public class SinkEventElement {
         builder.append(']');
         return builder.toString();
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.deepHashCode(args);
+        result = prime * result + Objects.hash(methodName);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        SinkEventElement other = (SinkEventElement) obj;
+        return Arrays.deepEquals(args, other.args) && Objects.equals(methodName, other.methodName);
+    }
 }
--- doxia-modules/doxia-module-markdown/src/test/resources/test.md
+++ doxia-modules/doxia-module-markdown/src/test/resources/test.md
@@ -2,3 +2,12 @@ Markdown test document
 ======================
 
 Placeholder for future expansion.
+
+- item1
+- item2
+- item3
+    - item31
+
+```
+code
+```
